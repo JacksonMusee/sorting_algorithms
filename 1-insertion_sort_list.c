aa69969f55b46b7c11e1a0c82bc484055e2fe67e
JacksonMusee <jacksonmutisya94@gmail.com>
2024-01-21 04:03:03 -0800

    Push

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,51 @@
+#include "sort.h"
+
+/**
+ *insertion_sort_list - Implments inserton sort
+ *
+ *@list: Pointer to a list
+ *
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *jst_b4;
+	listint_t *resume;
+	bool swapped = true;
+	listint_t *mvn_nd = *list;
+
+	if (list == NULL)
+		exit(EXIT_SUCCESS);
+
+	while (mvn_nd != NULL)
+	{
+		resume = mvn_nd->next;
+		jst_b4 = mvn_nd->prev;
+		while (jst_b4 != NULL && swapped)
+		{
+			swapped = false;
+			if (jst_b4->n > mvn_nd->n)
+			{
+				jst_b4->next = mvn_nd->next;
+				if (jst_b4->next)
+					jst_b4->next->prev = jst_b4;
+				if (jst_b4->prev != NULL)
+				{
+					jst_b4->prev->next = mvn_nd;
+				}
+				else
+				{
+					*list = mvn_nd;
+				}
+				mvn_nd->prev = jst_b4->prev;
+				mvn_nd->next = jst_b4;
+				jst_b4->prev = mvn_nd;
+				swapped = true;
+				print_list(*list);
+			}
+			jst_b4 = mvn_nd->prev;
+		}
+		mvn_nd = resume;
+		swapped = true;
+	}
+}
