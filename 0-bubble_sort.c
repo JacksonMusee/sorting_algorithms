01b7624081d7e19c662e3d5724b50feb662763de
JacksonMusee <jacksonmutisya94@gmail.com>
2024-01-21 00:45:21 -0800

    Push

---
+++ 0-bubble_sort.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+
+/**
+ *bubble_sort - Implements bubble sort
+ *
+ *@array: Pointer to the array to sort
+ *@size: Array size
+ *
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	int tmp;
+	int *arr = array;
+	bool swapped = false;
+	bool start = true;
+	size_t i = 0;
+
+	while (swapped || start)
+	{
+		start = false;
+		swapped = false;
+
+		while (i < size - 1)
+		{
+			if (*(arr + i) > *(arr + (i + 1)))
+			{
+				tmp = arr[i];
+				arr[i] = *(arr + (i + 1));
+				arr[i + 1] = tmp;
+				swapped = true;
+				print_array(arr, size);
+			}
+			i++;
+		}
+		i = 0;
+	}
+}
