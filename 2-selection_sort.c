8ce04518eccfae28cacc6e4a04798a0af85c767d
JacksonMusee <jacksonmutisya94@gmail.com>
2024-01-22 14:10:29 -0800

    Push

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,11 +10,15 @@
 
 void selection_sort(int *array, size_t size)
 {
-	int min_val = array[0];
+	int min_val;
 	size_t min_val_idx = 0;
 	size_t actv_idx = 1;
 	size_t start_idx = 0;
 
+	if (array == NULL)
+		return;
+
+	min_val = array[min_val_idx];
 	while (start_idx < size)
 	{
 		while (actv_idx < size)
